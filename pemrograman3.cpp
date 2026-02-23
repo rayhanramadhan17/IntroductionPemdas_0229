4822cdf6bb8228f984ce842ccdd1603d00af56a8
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-02-23 21:01:46 +0700

    variabel2

---
+++ pemrograman3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// variabel global
+int sks;
+
+int main() {
+    // variabel lokal
+    int umur;
+    umur = 18;
+
+    cout << "Halo Mas Reyy" << endl;
+    cout << 10 << endl;
+    cout << umur;
+}
